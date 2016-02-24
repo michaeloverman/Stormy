0a46f0d5da3afd13d311d9e41c3c4ebc5cd1e3c7
Michael Overman <overman.michael@gmail.com>
2016-02-23 23:06:13 -0500

    Finished app according to Treehouse course. Should try to: add detail view of daily/hourly forecasts; dynamically find users location

--- app/src/main/java/com/overman/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/overman/stormy/ui/HourlyForecastActivity.java
@@ -1,9 +1,9 @@
 package com.overman.stormy.ui;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
-import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
@@ -16,7 +16,7 @@ import java.util.Arrays;
 import butterknife.Bind;
 import butterknife.ButterKnife;
 
-public class HourlyForecastActivity extends ActionBarActivity {
+public class HourlyForecastActivity extends Activity {
 
     private Hour[] mHours;
 
@@ -32,7 +32,7 @@ public class HourlyForecastActivity extends ActionBarActivity {
         Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
         mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
 
-        HourAdapter adapter = new HourAdapter(mHours);
+        HourAdapter adapter = new HourAdapter(this, mHours);
         mRecyclerView.setAdapter(adapter);
 
         RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
