f191c69c8efc1121e55ff8a35027cda6e346c4eb
Michael Overman <overman.michael@gmail.com>
2016-02-23 22:30:46 -0500

    Hourly forecast up and running.

--- app/src/main/java/com/overman/stormy/ui/HourlyForecastActivity.java
+++ app/src/main/java/com/overman/stormy/ui/HourlyForecastActivity.java
@@ -1,15 +1,43 @@
 package com.overman.stormy.ui;
 
-import android.app.ListActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 import com.overman.stormy.R;
+import com.overman.stormy.adapters.HourAdapter;
+import com.overman.stormy.weather.Hour;
 
-public class HourlyForecastActivity extends ListActivity {
+import java.util.Arrays;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+public class HourlyForecastActivity extends ActionBarActivity {
+
+    private Hour[] mHours;
+
+    @Bind(R.id.recyclerView) RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hourly_forecast);
+        ButterKnife.bind(this);
+
+        Intent intent = getIntent();
+        Parcelable[] parcelables = intent.getParcelableArrayExtra(MainActivity.HOURLY_FORECAST);
+        mHours = Arrays.copyOf(parcelables, parcelables.length, Hour[].class);
+
+        HourAdapter adapter = new HourAdapter(mHours);
+        mRecyclerView.setAdapter(adapter);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(layoutManager);
+
+        mRecyclerView.setHasFixedSize(true);
     }
 }
--- app/src/main/java/com/overman/stormy/weather/Hour.java
+++ app/src/main/java/com/overman/stormy/weather/Hour.java
@@ -1,15 +1,24 @@
 package com.overman.stormy.weather;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
 /**
  * Created by Michael on 2/21/2016.
  */
-public class Hour {
+public class Hour implements Parcelable {
     private long mTime;
     private String mSummary;
     private double mTemperature;
     private String mIcon;
     private String mTimezone;
     private double mPrecipProbability;
+
+    public Hour() { }
 
     public int getPrecipProbability() {
         return (int) Math.round(mPrecipProbability * 100);
@@ -27,6 +36,13 @@ public class Hour {
         mTime = time;
     }
 
+    public String getHour() {
+        SimpleDateFormat formatter = new SimpleDateFormat("h a");
+        formatter.setTimeZone(TimeZone.getTimeZone(mTimezone));
+        Date dateTime = new Date(mTime * 1000);
+        return formatter.format(dateTime);
+    }
+
     public String getSummary() {
         return mSummary;
     }
@@ -35,8 +51,8 @@ public class Hour {
         mSummary = summary;
     }
 
-    public double getTemperature() {
-        return mTemperature;
+    public int getTemperature() {
+        return (int) Math.round(mTemperature);
     }
 
     public void setTemperature(double temperature) {
@@ -51,6 +67,10 @@ public class Hour {
         mIcon = icon;
     }
 
+    public int getIconId() {
+        return Forecast.getIconId(mIcon);
+    }
+
     public String getTimezone() {
         return mTimezone;
     }
@@ -58,4 +78,40 @@ public class Hour {
     public void setTimezone(String timezone) {
         mTimezone = timezone;
     }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeLong(mTime);
+        dest.writeString(mSummary);
+        dest.writeDouble(mTemperature);
+        dest.writeDouble(mPrecipProbability);
+        dest.writeString(mIcon);
+        dest.writeString(mTimezone);
+    }
+
+    private Hour(Parcel in) {
+        mTime = in.readLong();
+        mSummary = in.readString();
+        mTemperature = in.readDouble();
+        mPrecipProbability = in.readDouble();
+        mIcon = in.readString();
+        mTimezone = in.readString();
+    }
+
+    public static final Creator<Hour> CREATOR = new Creator<Hour>() {
+        @Override
+        public Hour createFromParcel(Parcel source) {
+            return new Hour(source);
+        }
+
+        @Override
+        public Hour[] newArray(int size) {
+            return new Hour[size];
+        }
+    };
 }
